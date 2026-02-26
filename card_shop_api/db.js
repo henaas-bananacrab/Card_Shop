fe6d18a4f3fe4f8482d85ce78f5c2ab1ff3f5802
henaas-bananacrab <henrikaas2008@gmail.com>
2026-02-26 10:38:02 +0100

    implementert fetchCustomers funksjonen for å hente customer data fra MySQL databasen

--- card_shop_api/db.js
+++ card_shop_api/db.js
@@ -1,1 +1,26 @@
 const mysql = require('mysql2/promise');
+
+// Create a connection 'pool' to the MySQL database
+async function fetchCustomers() {
+    try {
+        const connection = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'jfkU_.,%¤#h734y38n(/T/JYGYTh',
+            database: 'card_shop_db'
+        });
+        console.log('Connected to database');
+
+        // Execute query to fetch all customers
+        const [rows, fields] = await connection.execute('SELECT * FROM `customers`');
+
+        // Close connection
+        await connection.end();
+
+        return rows;
+    } catch (error) {
+        console.error('Error connecting to database:', error);
+    }
+}
+
+module.exports = fetchCustomers;
